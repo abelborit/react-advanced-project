12cfad0cfdc30eda654021a05740bf102b87be8f
Abel Borit <abelboritguitton@gmail.com>
2023-08-25 14:39:20 -0500

    [create] 02-react-ts-app

---
+++ react-projects/02-react-ts-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { NavigationLayout } from "./routes/NavigationLayout";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <h1>React + TypeScript (App)</h1> */}
+      <NavigationLayout></NavigationLayout>
+    </>
+  );
+}
+
+export default App;
