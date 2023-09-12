db80ece1797c1ec564455daf933cc7d3d4aefebf
Abel Borit <abelboritguitton@gmail.com>
2023-09-12 14:45:30 -0500

    [add] control props component pattern version 2

--- react-projects/02-react-ts-app/src/02-component-patterns/hooks/useProduct.ts
+++ react-projects/02-react-ts-app/src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { HandleChangeArgs, ProductInterface } from "../interfaces";
 
 /* esta interface es más que todo para los argumentos que va a recibir este custom hook para poder manejar todo el estado en sí, aquí se estaría mandando todo el producto, la función para cambiar la cantidad y la cantidad como tal */
@@ -17,15 +17,7 @@ export const useProduct = ({
   /* usando el useState significa que se tienen diferentes estados porque en pocas palabras se puede decir que hay diferentes componentes o instancias de estos componentes y por ende diferentes estados (por más que sea el mismo componente en cuanto a código) y esto significa que cada uno funciona por separado solo que se está sincronizando con el valueProps. Se puede dejar así ya que funciona normal y no hay ningún problema pero no se estaría usando el patrón de Control Props */
   const [counterState, setCounterState] = useState(valueProps);
 
-  /* si el onChangeProps y valueProps existen entonces el componente useProduct sería controlado. Entonces se tendrá una referencia para saber si mi componente está siendo controlado y se utilizará un useRef por que de esta forma no se tiene que re-rendirizar los componentes en caso cambien. Se coloca !!onChangeProps para que me devuelva un true en este caso para saber que sí es controlado porque si se coloca así onChangeProps entonces podría ser un undefined */
-  const isControlled = useRef(!!onChangeProps);
-
   const increaseOrDecreaseBy = (valueArgs: number = 1) => {
-    if (isControlled.current /* && onChangeProps */) {
-      /* condición para saber si está siendo controlado entonces no quiero que mi estado del contador se mande a cambiar con en las líneas de abajo fuera del if(). Se coloca onChangeProps! con el signo de admiración al final para decirle a TypeScript que efectivamente tendrá un valor siempre en el onChangeProps y no será un undefined porque ya se está haciendo la validación de isControlled.current. Si se sacara el signo ! entonces daría un warning diciendo que onChangeProps puede ser undefined. Otra forma puede ser que en la condición se coloque (isControlled.current && onChangeProps) pero eso afectaría un poco a javascript y colocando el signo ! sería un poquito más eficiente */
-      return onChangeProps!({ quantity: valueArgs, product });
-    }
-
     const newValue = Math.max(counterState + valueArgs, 0);
     /* Math.max() para seleccionar el valor máximo porque por ejemplo si el counterState = -1 entonces debería reemplazarse por 0 ya que ese será el líminte inferior */
     setCounterState(newValue);
--- react-projects/02-react-ts-app/src/02-component-patterns/hooks/useShoppingCart.ts
+++ react-projects/02-react-ts-app/src/02-component-patterns/hooks/useShoppingCart.ts
@@ -16,37 +16,22 @@ export const useShoppingCart = () => {
     quantity,
   }: HandleChangeArgs) => {
     setShoppingCartState((prevShoppingCartState) => {
-      /* el estado de shoppingCartState tiene que ser el que predomine y lo del count sería solo para indicar si es +1 o si es -1 en caso aumente o decremente */
-      /* aquí hay que hacer varias validaciones como saber si el producto existe y si existe entonces aumentarle +1 y si no existe entonces crearlo y así otras validaciones más. Entonces se creará una constante productInCart que será el producto con el id que se le está pasando pero la primera vez puede que sea nulo entonces eso significa que el producto no existe entonces se va a crearlo y la cantidad se coloca en 0 porque no hay ningún producto creado en ese momento. Con esa constante se eliminan varias validaciones */
-      const productInCart: ProductInCartInterface = prevShoppingCartState[
-        product.id
-      ] || { ...product, quantity: 0 };
+      console.log({ quantity });
 
-      /* validar para saber si la cantidad es mayor a cero y si es así entonces significa que puedo incrementarlo */
-      if (Math.max(productInCart.quantity + quantity, 0) > 0) {
-        productInCart.quantity += quantity;
+      /* si la quantity del producto que se recibe es igual a cero */
+      if (quantity === 0) {
+        /* eliminación de un objeto mediante desestructuración (podría ser más facil si fuera un array porque se aplica filter o algo similar pero en este caso es un objeto) Entonces se hará la desestructuración de propiedades computadas donde [product.id]: toDelete será el elemento que quiero eliminar del objeto (es el producto que se le manda a través de argumento en la función) y ...restArgs será el resto de los productos y al final todo ese restante de argumentos será lo único que se retorna y de esa forma no se trabaja directamente mutando el estado sino generando un nuevo estado y así es como se trabaja con react */
+        /* : toDelete es como un alias para que no de error en la desestructuración */
+        const { [product.id]: toDelete, ...restArgs } = prevShoppingCartState;
 
-        return { ...prevShoppingCartState, [product.id]: productInCart };
+        return { ...restArgs };
+        // return restArgs; /* puede ser así también porque ya se está desestructurando arriba */
       }
 
-      /* borrar el producto */
-      const { [product.id]: toDelete, ...restArgs } = prevShoppingCartState;
-      return restArgs;
-
-      // /* si la quantity del producto que se recibe es igual a cero */
-      // if (quantity === 0) {
-      //   /* eliminación de un objeto mediante desestructuración (podría ser más facil si fuera un array porque se aplica filter o algo similar pero en este caso es un objeto) Entonces se hará la desestructuración de propiedades computadas donde [product.id]: toDelete será el elemento que quiero eliminar del objeto (es el producto que se le manda a través de argumento en la función) y ...restArgs será el resto de los productos y al final todo ese restante de argumentos será lo único que se retorna y de esa forma no se trabaja directamente mutando el estado sino generando un nuevo estado y así es como se trabaja con react */
-      //   /* : toDelete es como un alias para que no de error en la desestructuración */
-      //   const { [product.id]: toDelete, ...restArgs } = prevShoppingCartState;
-
-      //   return { ...restArgs };
-      //   // return restArgs; /* puede ser así también porque ya se está desestructurando arriba */
-      // }
-
-      // return {
-      //   ...shoppingCartState,
-      //   [product.id]: { ...product, quantity },
-      // };
+      return {
+        ...shoppingCartState,
+        [product.id]: { ...product, quantity },
+      };
     });
   };
 
